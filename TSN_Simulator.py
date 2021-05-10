dda7d0cce08ff9e41ce60c654dd59bccaeea9b81
Dean Laptop <dean6132@gmail.com>
2021-05-10 18:46:07 +0100

    update requirements and tidy up files

--- TSN_Simulator.py
+++ TSN_Simulator.py
@@ -24,6 +24,7 @@ g_node_id_dict = {}
 
 
 
+
 ##################################################
 ############## DEFINE NODES CLASSES ##############
 ##################################################
@@ -32,15 +33,14 @@ g_node_id_dict = {}
 class Node:
 
     def __init__(self, id, name="unnamed"):
-        self.node_type = str(self.__class__.__name__)
-        self.id = id
+        self.node_type = str(self.__class__.__name__)  # this will be the same as the class name for each node
+        self.id = id  # unique
         self.name = name
         self.ingress_traffic = []
         self.egress_traffic = []
 
 
-
-    def toString(self):
+    def to_string(self):
         return str(self.node_type+" (ID: "+str(self.id)+") "+str(self.name))
 
 
@@ -62,7 +62,7 @@ class End_Station(Node):
 
     # somehow loop this or it activates when traffic has been sent to this node
     def on_receive(self):
-        # maybe if ingress queue not empty do something here
+        # maybe if ingress queue not empty do something here as a way of looping
         print("Received")
         pass
 
@@ -83,19 +83,19 @@ class Switch(Node):
         self.local_routing_table = -1  # to be set
         self.queue = -1  # to be set
 
-        # TODO : queue_def is parsed outside of this object and is of type queue, each switch should be different
-        # TODO : routing table should be passed in and then parsed in the object to get local routing table
+        # TODO : queue_def is parsed outside of this object and is of type queue, each switch can be different
 
 
     # sends the traffic to destination from route in routing table
     def forward(self, destination):
         self.packets_transmitted += 1
+        # TODO : implement destination lookup in local routing table and add to egress queue
 
 
     # call this a lot
     def recalculate_delay(self):
-        pass
         # recalculate the delay based on current packet in and out time
+        pass
 
 
     # setters
@@ -113,7 +113,8 @@ class Controller(Switch):
 
     def __init__(self, id, name="unnamed"):
         super().__init__(id, name)
-        self.routing_table = -1  # to be set
+        self.routing_table = -1  # the entire routing table is stored in this object as a dict
+        # TODO : set queue type here
 
 
     def monitor_traffic(self):
@@ -126,11 +127,8 @@ class Controller(Switch):
         pass
 
 
-    def set_routing_table(self, g_routing_table):
-        self.routing_table = g_routing_table
-
-#    def set_local_routing_table(self, routing_table_def):  # may not need this. May not need both a local routing and full routing
-#        super().set_routing_table(routing_table_def)
+    def set_routing_table(self, main_routing_table):
+        self.routing_table = main_routing_table  # the entire routing table is stored in this object
 
 
 
@@ -145,7 +143,7 @@ class Traffic():
     def __init__(self, source, destination):
         # get IDs
         self.source = source
-        self.destination = destination  # list
+        self.destination = destination  # list. May not need to be initialised
 
 
 
@@ -162,19 +160,19 @@ class Packet(Traffic):
 
 # ST traffic type
 class ST(Packet):
+    # for this type of traffic we use the GCL so need to show this somehow
 
     def __init__(self, source, destination, priority, delay_jitter_constraints, period, deadline, \
                  name="unnamed", offset="0"):
-        super().__init__(source, destination, 1, name, offset)
+        super().__init__(source, destination, 1, name, offset)  # priority 1
         self.delay_jitter_constraints = delay_jitter_constraints
         self.period = period
         self.hard_deadline = deadline
 
-        # for this type of traffic we use the GCL so need to show this somehow
 
 
-
-# non-st traffic type
+# non-st traffic type. Abstraction. No traffic should directly be NonST
+# commonly called sporadic soft
 class NonST(Packet):
 
     def __init__(self, source, destination, priority, minimal_inter_release_time, delay_jitter_constraints, \
@@ -191,7 +189,7 @@ class Sporadic_Hard(NonST):
 
     def __init__(self, source, destination, priority, minimal_inter_release_time, delay_jitter_constraints, \
                  period, deadline, name="unnamed", offset="0"):
-        super().__init__(source, destination, 2, minimal_inter_release_time, delay_jitter_constraints, period, name, offset)
+        super().__init__(source, destination, 2, minimal_inter_release_time, delay_jitter_constraints, period, name, offset)  # priority 2
         self.hard_deadline = deadline
 
 
@@ -201,7 +199,7 @@ class Sporadic_Soft(NonST):
 
     def __init__(self, source, destination, priority, minimal_inter_release_time, delay_jitter_constraints, \
                  period, deadline, name="unnamed", offset="0"):
-        super().__init__(source, destination, 3, minimal_inter_release_time, delay_jitter_constraints, period, name, offset)
+        super().__init__(source, destination, 3, minimal_inter_release_time, delay_jitter_constraints, period, name, offset)  # priority 3
         self.soft_deadline = deadline
 
 
@@ -211,7 +209,7 @@ class Best_Effort(NonST):
 
     def __init__(self, source, destination, priority, minimal_inter_release_time, delay_jitter_constraints, \
                  period, name="unnamed", offset="0"):
-        super().__init__(source, destination, 4, minimal_inter_release_time, delay_jitter_constraints, period, name, offset)
+        super().__init__(source, destination, 4, minimal_inter_release_time, delay_jitter_constraints, period, name, offset)  # priority 4
 
 
 
@@ -252,8 +250,10 @@ class Queue:
     def acceptance_test(self, frame):
         return True
 
+
     def packet_ingress(self, packet):
         pass
+
 
     def packet_egress(self, packet):
         pass
@@ -321,16 +321,15 @@ def error_check_switch(switch):
             return -1
 
         # child switch
-        if(node.tag == "Switch"):
-            if(error_check_switch(node) == -1):  # recursively check for errors on this switch and its children switches if any
+        if node.tag == "Switch":
+            if error_check_switch(node) == -1:  # recursively check for errors on this switch and its children switches if any
                 return -1
-            if(int(node.get("unique_id")) in g_node_id_dict):  # double check none of its children has its ID before adding it
+            if int(node.get("unique_id")) in g_node_id_dict:  # double check none of its children has its ID before adding it
                 print("ERROR: Duplicate ID found (ID:", node.get("unique_id")+")")
                 return -1
             switch_count += 1
             switch_node = Switch(int(node.get("unique_id")), node.get("name"))  # create node
             g_node_id_dict[int(node.get("unique_id"))] = switch_node  # switch and its children are error free, add its ID to list
-
 
     return 1
 
@@ -342,7 +341,7 @@ def parse_network_topo(f_network_topo):
 
     global g_node_id_dict
 
-    # parse file and get root
+    # parse XML file and get root
     parser = etree.XMLParser(ns_clean=True)
     tree = etree.parse(f_network_topo, parser)
     root = tree.getroot()
@@ -351,19 +350,19 @@ def parse_network_topo(f_network_topo):
     ### Get info into easy to read variables and do some error checking
     ## Controller
     controller = root[0]
-    if(len(root) != 1):  # only 1 controller permitted
+    if len(root) != 1:  # only 1 controller permitted
         print("ERROR: Invalid controller count")
         return -1
-    if(controller.tag != "Controller"):  # controller must be called "Controller" at depth 1 in the XML
+    if controller.tag != "Controller":  # controller must be called "Controller" at depth 1 in the XML
         print("ERROR: Controller missing or at incorrect depth")
         return -1
-    if(len(controller.keys()) != 2):  # controller must only have 2 attributes called "name" and "unique_id"
+    if len(controller.keys()) != 2:  # controller must only have 2 attributes called "name" and "unique_id"
         print("ERROR: Controler has too many attributes")
         return -1
     if( ("unique_id" not in controller.keys()) or ("name" not in controller.keys()) ):
         print("ERROR: Invalid controller attribute names")
         return -1
-    if(controller.get("unique_id") != str(0)):
+    if controller.get("unique_id") != str(0):
         print("ERROR: Controller ID must be 0")
         return -1
     controller_node = Controller(0, controller.get("name"))  # create controller node with id 0 and name from file
@@ -372,16 +371,16 @@ def parse_network_topo(f_network_topo):
 
     ## Switches
     switch_count = len(root[0])
-    if(switch_count < 1):  # must be at least 1 switch
+    if switch_count < 1:  # must be at least 1 switch
         print("ERROR: There must be at least 1 switch")
         return -1
     for switch in controller:
-        if(switch.tag != "Switch"):  # make sure we only have switches and not end stations connected to the controller
+        if switch.tag != "Switch":  # make sure we only have switches and not end stations connected to the controller
             print("ERROR: Invalid switch node tag connected to the Controller")
             return -1
-        if(error_check_switch(switch) == -1):  # recursively check for errors on this switch and its children switches if any
+        if error_check_switch(switch) == -1:  # recursively check for errors on this switch and its children switches if any
             return -1
-        if(int(switch.get("unique_id")) in g_node_id_dict):  # double check none of its children has its ID before adding it
+        if int(switch.get("unique_id")) in g_node_id_dict:  # double check none of its children has its ID before adding it
             print("ERROR: Duplicate ID found (ID:", switch.get("unique_id")+")")
             return -1
         switch_node = Switch(int(switch.get("unique_id")), switch.get("name"))  # create node
@@ -390,8 +389,8 @@ def parse_network_topo(f_network_topo):
 
     ### Finish up
     # check we havent exceeded the max count
-    if(len(g_node_id_dict) > MAX_NODE_COUNT):
-        print("ERROR: Too many nodes (MAX:", str(MAX_NODE_COUNT)+")")
+    if len(g_node_id_dict) > MAX_NODE_COUNT:
+        print("ERROR: Too many nodes:", len(g_node_id_dict), "(MAX:", str(MAX_NODE_COUNT)+")")
         return -1
 
     return 1
@@ -408,18 +407,16 @@ def parse_routing_table(f_network_topo):
     tree = etree.parse(f_network_topo, parser)
     root = tree.getroot()
 
+
     # get a list of all switches and end stations from the network topo
     switch_list = []
     end_station_list = []
     for key in g_node_id_dict:
-        if(g_node_id_dict[key].node_type == "Switch"):
+        if g_node_id_dict[key].node_type == "Switch":
             switch_list.append(key)
         if g_node_id_dict[key].node_type == "End_Station":
             end_station_list.append(key)
     switch_list.append(0)  # controller is also a switch
-    #print("SWITCHES:", switch_list)
-    #print("END STATIONS:", end_station_list)
-
 
     # for every switch in the XML, add all its children to global routing table dictionary to be altered later
     for node in root.iter("Switch"):
@@ -434,32 +431,28 @@ def parse_routing_table(f_network_topo):
 
         for switch_id in routing_table:  # for each switch in the routing table
             out_tuple = []  # prepare list to keep track of any hops for the entire switch we are checking
-            switches_to_delete = []  # prepare list of indexes that we are to delete upon completion of the scan
+            switches_to_delete = []  # prepare list of elements that we are to delete upon completion of the scan
 
             for child_index in range(len(routing_table[switch_id])):  # check every child of the current switch
-                if routing_table[switch_id][child_index][0] in switch_list:  # if the child of a switch is another switch we have found a hop switch
+                if routing_table[switch_id][child_index][0] in switch_list:  # if the child of a switch is another switch we have found a hop point
                     changes += 1  # increase change count
-                    #print("Found a switch in children of switch:", switch_id)
-                    #print("child switch is", routing_table[switch_id][child_index][0], "at pos", child_index)
-                    #print("Switch pre-change:", routing_table[switch_id])
 
                     # insert hop entries from child switch
-                    for node in routing_table[routing_table[switch_id][child_index][0]]:  # for each node connected to the child switch
-                        #print(node, routing_table[switch_id][child_index])
+                    for node in routing_table[routing_table[switch_id][child_index][0]]:  # for each node connected to the child hop switch
 
-                        # build the translation (in the form (end_station_id, hop_switch_id)
+                        # build the translation in the form (end_station_id, hop_switch_id)
                         part_out_tuple = []
                         part_out_tuple.append(node[0])  # get end_station_id from child switch
 
-                        if(routing_table[switch_id][child_index][1] == switch_id):
-                            part_out_tuple.append(routing_table[switch_id][child_index][0])  # insert hop switch_id
+                        if routing_table[switch_id][child_index][1] == switch_id:
+                            part_out_tuple.append(routing_table[switch_id][child_index][0])  # insert direct hop switch_id
                         else:
-                            part_out_tuple.append(routing_table[switch_id][child_index][1])  # insert hop switch_id
+                            part_out_tuple.append(routing_table[switch_id][child_index][1])  # insert indirect hop switch_id
 
                         # append translation to the new lists so we do not modify the live list
                         out_tuple.append(tuple(part_out_tuple))
-
                     switches_to_delete.append(routing_table[switch_id][child_index])  # remember the broken entry to delete later
+
 
             # finally modify the live switch dict to reflect new changes
             for element in switches_to_delete:
@@ -467,53 +460,49 @@ def parse_routing_table(f_network_topo):
             for hop in out_tuple:
                 routing_table[switch_id].append(hop)  # insert new hop entries into the list
 
+
         # we can leave the while loop if we make no changes
-        if(changes == 0):
+        if changes == 0:
             break
 
 
-
-    # next make sure every end station is included in the routing table. If any are missing it means they are not children, so add the parent as an entry
+    # next make sure every end station is included in the routing table. If any are missing it means they are NOT children, so add the parent as an entry
     for switch_id in routing_table:
         missing_end_stations = end_station_list.copy()  # create list of all end stations
 
-        for child_id in routing_table[switch_id]:  # have to check each child of the switch
+        for child_id in routing_table[switch_id]:  # check each child of the switch
             if child_id[0] in missing_end_stations:
                 missing_end_stations.remove(child_id[0])  # remove stations that are present form the list so we have a list of missing end station ids
-
 
         # add entries for each end station that is missing
         for node in root.iter("Switch"):
             if int(node.get("unique_id")) == switch_id:  # find switch in XML so we can get the parent
-
                 for end_station in missing_end_stations:
-                    routing_table[switch_id].append(tuple((end_station, int(node.getparent().get("unique_id")))))  # append tuple of (es_id, sw_parent) to routing
+                    routing_table[switch_id].append(tuple( (end_station, int(node.getparent().get("unique_id"))) ))  # append tuple of (es_id, sw_parent) to routing
 
 
     # error checking
     # each switch should have the same amount of children representing every end station
     for key in routing_table:
-        if(len(routing_table[key]) != len(end_station_list)):
+        if len(routing_table[key]) != len(end_station_list):
             print("ERROR: Entry \""+key+"\"", "in the global routing table has an incorrect amount of children")
             return -1
 
     # each switch should be present in the routing table
-    if(len(routing_table) != len(switch_list)):
+    if len(routing_table) != len(switch_list):
         print("ERROR: Incorrect amount of switches present in the global routing table")
         return -1
 
 
     # populate switch objects with their appropriate routing table
     for key in g_node_id_dict:
-        if(key in routing_table):
+        if key in routing_table:
             if key == 0:  # add entire routing table to the controller
                 g_node_id_dict[key].set_routing_table(routing_table)
                 g_node_id_dict[key].set_local_routing_table(routing_table[key])
 
             else:  # only add local routing table to switches
                 g_node_id_dict[key].set_local_routing_table(routing_table[key])
-
-            #print("switch:", key, "table set:", g_node_id_dict[key].routing_table)
 
     return 1
 
@@ -528,6 +517,7 @@ def parse_routing_table(f_network_topo):
 
 # specify file paths and names
 network_topo_file  = "example_network_topology.xml"
+
 
 
 ### Parse given files
